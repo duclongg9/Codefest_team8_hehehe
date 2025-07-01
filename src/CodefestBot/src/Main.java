5eea23705d3b40311591e95df9057962228ef101
Ngo Thuong Huyen <ngothuonghuyen2@gmail.com>
2025-07-01 17:45:32 +0700

    test

--- src/CodefestBot/src/Main.java
+++ src/CodefestBot/src/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Main {
     private static final String SERVER_URL = "https://cf25-server.jsclub.dev";
-    private static final String GAME_ID = "150270";
+    private static final String GAME_ID = "143238";
     private static final String PLAYER_NAME = "Team8-Bot1";
     private static final String SECRET_KEY = "sk-GdaqH5KQTMOJv3saId8OGQ:Tl-3murL582L5qOgCMqYu83NTXWKbzfHoMmzyUc0A7hMYh4ah3qs0ysgn_lz6tLT5QmvxPgxv0f52qTfB9IrDA";
 
